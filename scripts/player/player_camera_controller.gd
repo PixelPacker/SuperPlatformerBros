43af39879713d024014671be0ec82f45798c2f3d
Kevin Stoffan <PixelPacker1@gmail.com>
2023-10-01 17:37:04 -0400

    Created Basic Player Controller
    Features:
    -Improved Jumping
    -Coyote Time
    -Basic Anim Controller (Likely to be replaced in the future but good for now)

--- scripts/player/player_camera_controller.gd
+++ scripts/player/player_camera_controller.gd
@@ -1,8 +1,18 @@
 extends Camera2D
 
-# Called when the node enters the scene tree for the first time.
+var camSmoothing = 0.1
+var cameraOffset : float = 2.5
+
 func _ready():
-	pass
+	GameManager.camera = self
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(_delta):
+	# Note for me on how to get the velocity variable because I'm dumb
+	# GameManager.player.get("variable")	
+	if(GameManager.player.facingLeft):
+		position.x = lerpf((position.x + -cameraOffset), (GameManager.player.position.x + -cameraOffset), camSmoothing)
+	elif(!GameManager.player.facingLeft):
+		position.x = lerpf((position.x + cameraOffset), (GameManager.player.position.x + cameraOffset), camSmoothing)
+		
