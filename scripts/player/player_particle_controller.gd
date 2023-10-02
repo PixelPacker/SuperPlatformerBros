6812865aa359d157cb604280badcdaba4f63dde3
Kevin Stoffan <PixelPacker1@gmail.com>
2023-10-01 23:52:54 -0400

    Basic Player Particle System Added
    Very basic, super protypey.
    To be cleaned up later

---
+++ scripts/player/player_particle_controller.gd
@@ -0,0 +1,16 @@
+extends GPUParticles2D
+
+const facingLeftLoc : Vector2 = Vector2(5, 15)
+const facingRightLoc : Vector2 = Vector2(-5, 15)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	var playerVel : Vector2 = GameManager.player.get("velocity")
+	if playerVel.x > 0 || playerVel.x < 0:
+		emitting = true
+		if GameManager.player.facingLeft:
+			position = facingLeftLoc
+		else:
+			position = facingRightLoc
+	else:
+		emitting = false
