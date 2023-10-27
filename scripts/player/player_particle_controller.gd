addbbe5c1885900b8fd7495b4cd0d10393e27d23
Kevin Stoffan <PixelPacker1@gmail.com>
2023-10-27 18:11:44 -0400

    Introduction of Music and SFX asset packs

--- scripts/player/player_particle_controller.gd
+++ scripts/player/player_particle_controller.gd
@@ -7,7 +7,7 @@ const facingRightLoc : Vector2 = Vector2(-5, 15)
 func _process(_delta):
 	var playerVel : Vector2 = GameManager.player.get("velocity")
 	# TODO: Improve this by preventing particles from being spawned while the player is in the air
-	if playerVel.x > 0|| playerVel.x < 0:
+	if playerVel.x != 0 && GameManager.player.is_on_floor():
 		emitting = true
 		if GameManager.player.facing_left:
 			position = facingLeftLoc
